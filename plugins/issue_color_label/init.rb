a6020b1b889475fc1f4448d6f94a955ef5dddf59
Bogdan Anton <contact@bogdananton.ro>
2014-07-26 02:50:09 +0300

    init

---
+++ plugins/issue_color_label/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :issue_color_label do
+  name 'Issue Color Label plugin'
+  author 'Bogdan Anton'
+  description 'This is a simple plugin that sets different labels for Status, Tracker and Priority columns'
+  version '0.0.1'
+  url 'http://github.com/bogdananton/Redmine-plugin-issueColorLabel'
+  author_url 'http://bogdananton.ro'
+end
