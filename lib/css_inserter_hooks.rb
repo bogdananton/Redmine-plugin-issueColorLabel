acd483e2e56f99385e439ee15dd589a34b72b4d3
sorins <sorin.stanila@avangate.com>
2014-07-30 17:08:11 +0300

    minor: fixed relative path for plugin and icon

--- lib/css_inserter_hooks.rb
+++ lib/css_inserter_hooks.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../app/helpers/icladmin_helper.rb'
 
 class CssInserterHooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = { })
-        $css_block = '<style type="text/css">' << "\n" << "#admin-menu .issue-color-label {background-image: url(images/changeset.png)} \n";
+        $css_block = '<style type="text/css">' << "\n" << "#admin-menu .issue-color-label {background-image: url(/images/changeset.png)} \n";
 
         if (IcladminHelper.isPluginEnabled)
             $settingsDisplay = IcladminHelper.getSettingsDisplay
